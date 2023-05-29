4f39da3298b6500658d54bae55177b23febaca0f
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-05-29 12:53:03 -0300

    Added  'Account Register' screen

--- taskManagement/index.php
+++ taskManagement/index.php
@@ -14,35 +14,38 @@
     <div class="container">
         <div class="loginContainer">
 
-            <h1 class="display-2 loginTitulo">login</h1>
+            <h1 class="display-2 loginTitulo">LOGIN</h1>
 
-            <div class="row">
+            <form>
+                <div class="row">
+    
+                    <div class="col-12">
+                        <label for="email">Email: </label>
+                    </div>
+                    <div class="col-12">
+                        <input type="text" id="email" class="form-control">
+                    </div>
+    
+                    <div class="col-12">
+                        <label for="email">Senha: </label>
+                    </div>
+                    <div class="col-12">
+                        <input type="password" id="senha" class="form-control">
+                    </div>                
+                </div>
+    
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <input type="submit"  class="btn btn-secondary mt-2" value="Entrar">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <a class="text-left signUp" href="../taskManagement/signup.php" >Não possui uma conta? Cadastre aqui </a>
+                    </div>
+                </div>
 
-                <div class="col-12">
-                    <label for="email">Email: </label>
-                </div>
-                <div class="col-12">
-                    <input type="text" id="email" class="form-control">
-                </div>
-
-                <div class="col-12">
-                    <label for="email">Senha: </label>
-                </div>
-                <div class="col-12">
-                    <input type="password" id="senha" class="form-control">
-                </div>                
-            </div>
-
-            <div class="row">
-                <div class="col-12 text-center">
-                    <input type="submit"  class="btn btn-secondary mt-2" value="Entrar">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <a class="text-left signUp">Não possui uma conta? Cadastre aqui </a>
-                </div>
-            </div>
+            </form>
 
         </div>
     </div>
