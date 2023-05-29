227916f4483a21effe2f5e669e9a1df7174c6cde
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-05-29 16:34:42 -0300

    Created mySQL Script + Updated Entity Relation Model

---
+++ dbTaskManagement.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE dbTaskManagement;
+USE dbTaskManagement;
+
+
+CREATE TABLE Usuarios (
+	idUsuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nome  VARCHAR(255),
+	email VARCHAR(255),
+	senha VARCHAR(255)
+)
+
+CREATE TABLE Administradores (
+    idUsuario INT,    
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
+    CONSTRAINT idAdministrador PRIMARY KEY(idUsuario)
+);
+
+CREATE TABLE Projetos (
+	idProjeto INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nome		VARCHAR(255),
+	descricao	VARCHAR(255),
+	dataInicio DATE,
+	dataTermino DATE
+)
+
+CREATE TABLE Usuarios_Projetos(
+	codUsuario INT,
+	codProjeto INT,
+
+	FOREIGN KEY (codProjeto) REFERENCES Projetos(idProjeto),
+	FOREIGN KEY (codUsuario) REFERENCES Usuarios(idUsuario),
+
+	CONSTRAINT idUsuarioProjeto PRIMARY KEY (codUsuario, codProjeto)
+)
+
+CREATE TABLE Tarefas (
+	idTarefa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+
+	codProjeto INT,
+	codUsuario INT,
+
+	descricao	VARCHAR(255),
+	dataInicio	DATE,
+	dataTermino	DATE,
+
+	Status INT CHECK (Status IN(1,2,3)),
+	FOREIGN KEY (codProjeto) REFERENCES Projetos(idProjeto),
+	FOREIGN KEY (codUsuario) REFERENCES Usuarios(idUsuario)
+)
+
+CREATE TABLE Comentarios (
+	idComentario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	codTarefa INT,
+	texto VARCHAR(255),
+	dataHora DATETIME,
+	
+	FOREIGN KEY (codTarefa) REFERENCES Tarefas(idTarefa)
+)
+
